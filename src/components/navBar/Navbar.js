a37f2158eeddfbe1f9b9ea0d81339a55895de97f
Tiko <tikxach20@gmail.com>
2020-04-30 00:47:19 +0400

    msg

---
+++ src/components/navBar/Navbar.js
@@ -0,0 +1,1 @@
+import React from "react";import AppBar from 'material-ui/AppBar'const NavBar = () => {    return (        <div>            <AppBar                title={'PixaBay Image Finder'}            />        </div>    )}export default NavBar
